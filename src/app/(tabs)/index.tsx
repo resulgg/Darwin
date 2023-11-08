eac8dd8ba3857816296dffa4c58950f3686f0510
Semih Basyigit <semihbasyigit@proton.me>
2023-11-08 13:43:25 +0300

    tab one name changed

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Tab Changed</Text>
       <View style={styles.separator} />
     </View>
   );
